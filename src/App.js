23a726b6a8915e7304bcdb86a6bafdbc3ee581ee
psemsari <41895689+psemsari@users.noreply.github.com>
2023-03-23 02:14:04 +0000

    feat: data fetch on gouv api

--- src/App.js
+++ src/App.js
@@ -1,27 +1,50 @@
-import { useState } from 'react';
+import { useEffect, useState} from 'react';
 import './App.css';
 
 function compare(dates, actual) {
   const comp = actual.toISOString().slice(0, 10)
-  for (let day of Object.keys(dates))
+  const lst = dates.dates
+  for (let day in lst)
   {
     if (day === comp)
-      return true
+      return lst[day]
   }
-  return false
+  return null
 }
 
-function IsFerie(dates) {
+function IsFerie(dates)
+{
   const actual = useState(new Date())[0]
-
-  if (compare(dates, actual))
+  const day = compare(dates, actual)
+  console.log(day)
+  if (day)
   {
-    return (<p>OUI</p>);
+    return (
+      <span>
+        {day}
+      </span>
+    );
   }
-  return (<p>NON</p>);
+  return (
+    <span>
+      NON
+    </span>
+  );
 }
 
 function App() {
+  const [data, setData] = useState({});
+  useEffect(() => {
+
+    const datafetch = async () => {
+      const response = await fetch("https://calendrier.api.gouv.fr/jours-feries/metropole/2023.json")
+      const json = await response.json()
+      setData(json)
+    }
+
+    datafetch()
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -36,7 +59,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <IsFerie dates={DATE}></IsFerie>
+          <IsFerie dates={data}></IsFerie>
           </a>
         </p>
       </header>
@@ -44,16 +67,16 @@ function App() {
   );
 }
 
-const DATE = {"2023-01-01": "1er janvier",
-              "2023-04-10": "Lundi de Pâques",
-              "2023-05-01": "1er mai",
-              "2023-05-08": "8 mai",
-              "2023-05-18": "Ascension",
-              "2023-05-29": "Lundi de Pentecôte",
-              "2023-07-14": "14 juillet",
-              "2023-08-15": "Assomption",
-              "2023-11-01": "Toussaint",
-              "2023-11-11": "11 novembre",
-              "2023-12-25": "Jour de Noël"}
+// const DATE = {"2023-01-01": "1er janvier",
+//               "2023-04-10": "Lundi de Pâques",
+//               "2023-05-01": "1er mai",
+//               "2023-05-08": "8 mai",
+//               "2023-05-18": "Ascension",
+//               "2023-05-29": "Lundi de Pentecôte",
+//               "2023-07-14": "14 juillet",
+//               "2023-08-15": "Assomption",
+//               "2023-11-01": "Toussaint",
+//               "2023-11-11": "11 novembre",
+//               "2023-12-25": "Jour de Noël"}
 
 export default App;
